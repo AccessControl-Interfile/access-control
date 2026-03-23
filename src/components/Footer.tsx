ed2e7945eca7823ad92a26b6867d33914dc30939
Access Control - Interfile <accesscontrol.interfile@gmail.com>
2026-03-23 12:27:48 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-full py-2 px-6 bg-white/50 backdrop-blur-sm border-t border-slate-200 mt-auto">
       <p className="text-center text-slate-400 text-[10px] font-medium tracking-wide">
-        © Developed by Lucas Cantão Gaspar de Souza
+        © Developed by Lucas Cantão
       </p>
     </footer>
   );
